23bf8263f39415ee8df7f904d1a03cc1af864771
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-04-21 21:37:40 -0500

    Se agrega carpeta de vistas y agregamos usuario fake para impresión en Cards

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-/* hola */
+/* ****************************************USERS SECTION ****************************************/
+
+// fake user
+let fakeUser = {
+    userId: Date.now(),
+    name: "Israel Salinas",
+    imageUrl: "https://media-exp1.licdn.com/dms/image/C4E03AQEKN_uf1kAPMw/profile-displayphoto-shrink_800_800/0/1550176229405?e=1624492800&v=beta&t=eP2Y1gyCpHtVPoB8Gz8lW8IO6G5ZKzSuyORxqB0EKxU",
+}
+
+// save users
+const saveUser = userData => {
+    $.ajax({
+        method:"POST",
+        url:"https://kodemiaproobs-default-rtdb.firebaseio.com/users/.json",
+        data:JSON.stringify( userData ),
+        success: response => {
+            console.log( response )
+        },
+        error: error => {
+            console.log( error )
+        }
+    })
+}
+
+
+
+/* ****************************************END USERS SECTION ****************************************/
+
+/* ****************************************POST SECTION ****************************************/
+
 // https://kodemiaproobs-default-rtdb.firebaseio.com/.json
 
 let post = {
@@ -57,9 +86,9 @@ const printPosts = postCollection => {
                     <div class="card-body" >
 
                         <div class="media mb-2">
-                            <img src="${imageUrl}" class="mr-2" alt="Generic placeholder image" style="width: 35px; height: 35px; border-radius: 50%;">
+                            <img src="${fakeUser.imageUrl}" class="mr-2" alt="Generic placeholder image" style="width: 35px; height: 35px; border-radius: 50%;">
                             <div class="media-body">
-                                <span style=" font-size: 0.8rem; display: block; margin-bottom: 4px;"><b>Yogini Bende</b></span>
+                                <span style=" font-size: 0.8rem; display: block; margin-bottom: 4px;"><b>${fakeUser.name}</b></span>
                                 <span class="gray-text" style=" font-size: 0.7rem; display: block; margin-bottom: 5px;">${createdDate}</span>
                             </div>
                         </div>
@@ -88,26 +117,7 @@ const printPosts = postCollection => {
     })
 }
 
+/* ****************************************END POST SECTION ****************************************/
 
 
-                // <div class="media mb-2">
-                //     <img src="https://picsum.photos/seed/picsum/200/300" class="mr-2" alt="Generic placeholder image" style="width: 35px; height: 35px; border-radius: 50%;">
-                //     <div class="media-body">
-                //         <span style=" font-size: 0.8rem; display: block; margin-bottom: 4px;"><b>Yogini Bende</b></span>
-                //         <span class="gray-text" style=" font-size: 0.7rem; display: block; margin-bottom: 5px;">Media heading</span>
-                //     </div>
-                // </div>
 
-                // <h5 class="card-title pl-5" style="font-size: 1.7rem;"><b>How to invalidate a JWT using a blacklist</b></h5>
-                // <p class="card-text gray-text pl-5" style="font-size: 0.9rem;">#Javascript #SQL #Technology #JS #CSS</p>
-                // <div class="col-12 d-flex">
-                    
-                //     <div class="col-4 col-md-8 d-flex align-items-center">
-                //         <i class="gray-text far fa-heart pl-4"></i><span class="d-none d-md-block pl-2 gray-text" style="font-size: 0.85rem;">25 reactions</span>
-                //         <i class="gray-text far fa-comment pl-4"></i><span class="d-none d-md-block pl-2 gray-text" style="font-size: 0.85rem;">4 comments</span>
-                //     </div>
-                //     <div class="col-8 col-md-4 d-flex justify-content-end align-items-center">
-                //         <span class="gray-text pr-2" style="font-size: 0.7rem;">4 min read</span>
-                //         <button class="btn-save">Save</button>
-                //     </div>
-                // </div>  
